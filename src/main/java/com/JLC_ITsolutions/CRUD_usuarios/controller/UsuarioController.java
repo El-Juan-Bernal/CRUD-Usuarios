524d038232192d28246022d65bbd07eb67780ba1
El-Juan-Bernal <ju.bernala@duocuc.cl>
2025-05-25 22:49:14 -0400

    Cambio de maping

--- src/main/java/com/JLC_ITsolutions/CRUD_usuarios/controller/UsuarioController.java
+++ src/main/java/com/JLC_ITsolutions/CRUD_usuarios/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +26,7 @@ import jakarta.validation.Valid;
 public class UsuarioController {
     @Autowired
     private UsuarioService service;
-    @PostMapping ("/id")
+    @DeleteMapping ("/id")
     public ResponseEntity<Void>eliminarusuario(@PathVariable Integer id){
         service.EliminarUsuario(id);
         return ResponseEntity.noContent().build();
