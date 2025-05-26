ced9d2d8d072428ab66777011387a33161068cc3
El-Juan-Bernal <ju.bernala@duocuc.cl>
2025-05-25 22:42:35 -0400

    first commit

---
+++ src/main/java/com/JLC_ITsolutions/CRUD_usuarios/models/UsuarioModel.java
@@ -0,0 +1,34 @@
+package com.JLC_ITsolutions.CRUD_usuarios.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cuentas")
+
+public class UsuarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer idUsuario;
+
+    @NotBlank
+    private String nombre;
+
+    @NotBlank
+    private String apellido;
+
+    @NotBlank
+    @Email
+    @Column(unique=true)
+    private String correo;
+
+    @NotBlank
+    private String contrasenia;
+
+}
